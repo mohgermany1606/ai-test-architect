74785d96db290d35bd93306cffc57c53ac7d509a
Mohini <mohgermany16061991@gmail.com>
2026-07-17 16:38:23 +0200

    Fix Playwright CI headless execution

--- src/browser.py
+++ src/browser.py
@@ -1,35 +1,44 @@
 from playwright.sync_api import sync_playwright
-
-from src.config_reader import ConfigReader
-from src.utils.logger import get_logger
+import os
 
 
 class Browser:
 
-    logger = get_logger(__name__)
+
+    def __init__(self):
+        self.playwright = None
+        self.browser = None
+
 
     def __enter__(self):
 
-        self.logger.info("Starting browser")
-
-        config = ConfigReader.load()
-
         self.playwright = sync_playwright().start()
 
+
+        headless = os.getenv(
+            "CI",
+            "false"
+        ) == "true"
+
+
         self.browser = self.playwright.chromium.launch(
-            headless=config["browser"]["headless"]
+            headless=headless
         )
+
 
         self.page = self.browser.new_page()
 
-        self.logger.info("Browser started successfully")
 
         return self.page
 
 
-    def __exit__(self, exc_type, exc_value, traceback):
 
-        self.logger.info("Closing browser")
+    def __exit__(
+        self,
+        exc_type,
+        exc_value,
+        traceback
+    ):
 
         self.browser.close()
 
